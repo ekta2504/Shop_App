fb88932cc83faf561ebb654a10588831cc0fe255
Ekta2504 <ekta.gangwani@seaflux.tech>
2023-03-06 11:00:53 +0530

    feat:redirecting

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './providers/auth.dart';
 import './screens/cart_screen.dart';
 import './screens/products_overview_screens.dart';
 import './screens/product_detail_screen.dart';
@@ -9,6 +10,7 @@ import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import '../screens/user_product_screen.dart';
 import '../screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,6 +20,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+        ChangeNotifierProvider.value(
           value: Products(),
         ),
         ChangeNotifierProvider.value(
@@ -25,22 +30,23 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(value: Orders()),
       ],
-      child: MaterialApp(
+      child: Consumer<Auth>(builder: (ctx,auth,_)=>MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: auth.isAuth?ProductsOverviewScreen():AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName:(ctx)=>CartScreen(),
-          OrdersScreen.routeName:(ctx)=>OrdersScreen(),
-          UserProductScreen.routeName:(ctx)=>UserProductScreen(),
-          EditProductScreen.routeName:(ctx)=>EditProductScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductScreen.routeName: (ctx) => UserProductScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
+      )
     );
   }
 }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,46 +4,39 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
+    print('building orders');
+  //  final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, i) => OrderItems(ordersData.orders[i]),
-            ),
+      body: FutureBuilder(future:Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx,dataSnapshot) {
+       if(dataSnapshot.connectionState == ConnectionState.waiting) {
+         return Center(child: CircularProgressIndicator(),);
+       }else {
+         if (dataSnapshot.error != null) {
+           //
+           Center(child: Text('An error occured!'),);
+         } else {
+           return Consumer<Orders>(builder: (ctx, ordersData, child) =>
+              ListView.builder(
+                 itemCount: ordersData.orders.length,
+                 itemBuilder: (ctx, i) => OrderItems(ordersData.orders[i]),
+               ),
+           );
+         }
+       }
+      },
+      ),
     );
   }
 }
